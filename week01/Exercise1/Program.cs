189dcb21162c3fe0adc73a050b5db82b3fb576cd
belloti44 <isaiahwambani@gmail.com>
2025-03-22 22:20:12 +0300

    week one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+         // Prompt for first name
+        Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        // Prompt for last name
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        // Display the output
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        
     }
 }
